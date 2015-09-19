75b2ba543f2566d271072a50c2c8ae3bf5fef95b
Sebastien Pierre <sebastien.pierre@gmail.com>
2015-09-19 11:57:05 -0400

    Response.normalizeHeaders: does not swallow double-headers

--- Sources/retro/core.py
+++ Sources/retro/core.py
@@ -1517,10 +1517,10 @@ class Response:
 		for i in range(len(headers) - 1, -1, -1):
 			k, v = headers[i]
 			k    = k.encode("ascii") if isinstance(k,unicode) else str(k)
-			if k not in output:
-				v = v.encode("ascii") if isinstance(v,unicode) else str(v)
-				output.append(k)
-				result.append((k,v))
+			#if k not in output:
+			v = v.encode("ascii") if isinstance(v,unicode) else str(v)
+			output.append(k)
+			result.append((k,v))
 		return result
 
 	def asWSGI( self, startResponse, charset=None ):
